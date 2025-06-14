d48a39d908bd7d422a140cb32446771c3d0c146c
mengsang <2673670601@qq.com>
2025-06-14 11:50:50 +0800

    update

--- js/ismobile.js
+++ js/ismobile.js
@@ -10,6 +10,6 @@ function isMobileDevice() {
 (() => {
     // 如果不是移动设备，重定向到提示页面或其他网址
     if (!isMobileDevice()) {
-        window.location.href = "./aaa.html"; // 替换为你的提示页面URL
+        window.location.href = "./404.html"; // 替换为你的提示页面URL
     }
 })()
